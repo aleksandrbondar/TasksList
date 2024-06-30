820a6681b661d3b0f2a2076def35461f49e8b18b
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-06-30 16:20:34 +0300

    v2.4

--- src/components/App/Style.ts
+++ src/components/App/Style.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import bg from '../../assets/img/bg.jpg';
 
 export const TasksApp = styled.div`
   font-family: 'Roboto', sans-serif;
-  background-image: url('./../../assets/img/bg.jpg');
+  background-image: url(${bg});
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
